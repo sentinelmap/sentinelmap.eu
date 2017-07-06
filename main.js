b5b8629e3f5dd3cea052f899a350d36051880c82
Carlo Palermo <carlo.palermo@hdati.com>
2017-07-06 18:42:38 +0200

    Update tangram, cleaner code

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 var satAttr = 'Modified <a href="https://scihub.copernicus.eu/">Copernicus</a>' +
     ' Sentinel data 2016 by ' +
     '<a href="https://www.sentinelmap.eu">SentinelMap</a>' ;
-
 var osmAttr = ' | © <a href="https://openstreetmap.org/copyright">OSM</a>' +
     ' contributors data by ' +
     '<a href="https://mapzen.com">Mapzen</a>' ;
+var attr = satAttr + osmAttr ;
 
-var Attr = satAttr + osmAttr ;
-
-var scLayer = Tangram.leafletLayer({
-    scene: 'scene@1.1.yaml',
-    attribution: Attr,
+var layer = Tangram.leafletLayer({
+    scene: {
+	import: ['global.yaml'],
+	global: {'labels_overlay': true,
+		 'roads_overlay': true},
+    },
+    attribution: attr,
 });
+var scene = layer.scene;
 
 var map = L.map('map' , {
     center: [ 48.92, 11.25],
-    //center: [ 47.04, 8.44],
     zoom: 6,
     maxZoom: 16,
     minZoom: 5,
-    layers: [scLayer],
+    layers: [layer],
     zoomControl: false
 });
-
-var baseLayers = {
-    "Sentinel-2": scLayer,
-};
-
 L.control.zoom({position: 'bottomright'}).addTo(map);
 
 var hash = new L.Hash(map);
@@ -35,13 +32,13 @@ var sidebar = L.control.sidebar('sidebar').addTo(map);
 
 // DEBUG
 /*
-scLayer.scene.subscribe({
+scene.subscribe({
     load: function (e) {
         console.log('scene loaded:', e);
     }
 });
 
-scLayer.scene.subscribe({
+scene.subscribe({
     view_complete: function () {
 	console.log('scene view complete');
     }
